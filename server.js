54219eb58571a98ff0f4bf0865ae8eaa3d20ce5e
ObjectIsAdvantag <steve.sfartz@gmail.com>
2016-04-10 08:17:04 +0100

    webhook structure set, next: parse incoming message

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
 var express = require('express');
-
 var app = express();
 
-var port = process.env.PORT || 8080;    
+var port = process.env.PORT || 8080;
+var webhook = process.env.WEBHOOK || "/webhook";
 
+// health endpoint
 app.get('/', function (req, res) {
-	res.json({ message: 'Your Cisco Spark bot is running' });  
+	res.json({ 'message': 'Your Cisco Spark bot is running', 'webhook': webhook, 'localport': port });
 });
 
+// use bodyParser to get the data from a POST
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// create webhook
+app.route(webhook)
+	.get(function(req, res) {
+		res.status(400).json({ message: 'Your Webhook is expecting an HTTP POST' });
+	})
+	.post(function(req, res) {
+		res.send('Add a book');
+	});
+
+// start api
 app.listen(port, function () {
   console.log('Cisco Spark bot started, running on port ' + port);
 });
