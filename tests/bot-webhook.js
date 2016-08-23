0d316f61f602adae403d450288c7272d7e168a08
ObjectIsAdvantag <steve.sfartz@gmail.com>
2016-08-23 15:11:12 +0200

    more testing & packaging

--- tests/bot-webhook.js
+++ tests/bot-webhook.js
@@ -2,6 +2,18 @@
  * Example of an integration that echoes new Cisco Spark messages to the console
  *    - as a REST Webhook, associated to a Bot Account
  *
+ * How to run
+ *     1. Create a bot account, retreive its Spark API token
+ *     2. Create a Spark Webhook
+ *      
+       curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer BOT_ACCOUNT_SPARK_API_TOKEN" 
+        -d '{
+              "name": "sparkbot-starterkit | test | bot-webhook",
+              "resource": "messages",
+              "event": "created",
+              "targetUrl": "https://mybot.localtunnel.me/webhook"
+            }' 
+        "https://api.ciscospark.com/v1/webhooks/"
  */
 var SparkBot = require("../sparkbot-starterkit");
 
